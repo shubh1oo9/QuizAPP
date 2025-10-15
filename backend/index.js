b1b00caba4b82a3571ebbcce73a76d183cc4cd24
Shubh Patel <shubhsp1009@gmail.com>
2025-10-15 07:22:23 +0530

    files added

---
+++ backend/index.js
@@ -0,0 +1,29 @@
+import cors from 'cors';
+import 'dotenv/config';
+import express from 'express';
+import { connectDB } from './config/db.js';
+import userRouter from './routes/userRoutes.js';
+import resultRouter from './routes/resultRoutes.js';
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors())
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Database Connection
+connectDB();
+
+// Routes
+app.use("/api/auth", userRouter);
+app.use("/api/results", resultRouter);
+
+app.get('/', (req, res) => {
+    res.send('API Working');
+});
+
+app.listen(port, () => {
+    console.log(`Server Started on http://localhost:${port}`);
+});
---
+++ backend/server.js
@@ -0,0 +1,29 @@
+import express from 'express'
+import cors from 'cors'
+import 'dotenv/config'
+import { connectDB } from './config/db.js';
+import userRouter from './routes/userRoutes.js';
+import resultRouter from './routes/resultRoutes.js';
+
+const app = express();
+const port = process.env.PORT || 4000;
+
+// MIDDELEWARE
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// DB
+connectDB();
+
+// ROUTES
+app.use('/api/auth', userRouter);
+app.use('/api/results', resultRouter);
+
+app.get('/', (req, res) => {
+  res.send('API WORKING');
+})
+
+app.listen(port, () => {
+  console.log(`Server Started on http://localhost:${port}`);
+})
